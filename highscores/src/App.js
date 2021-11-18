ed6da60dced910b82412972b5c605c882d0130e4
haseeb3927 <haseeb3927@gmail.com>
2021-11-18 15:15:56 +0000

    Some fixes

--- highscores/src/App.js
+++ highscores/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Heading from "./Heading";
-import CountryTable from "./Highscore-Country-Table";
+import CountryTable from "./HighScore-Country-Table";
 import AllCountryScores from "./scores";
 
 function App() {
   return (
     <div className="App">
       <Heading />
-
       {AllCountryScores.map((country, index) => {
         return <CountryTable key={index} value={country} />;
       })}
